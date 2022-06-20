c35565d1e1f75c7016cfc0c87210c981fdd69187
IZenithy <ahmedmohammed2429@gmail.com>
2022-06-20 18:09:41 +0300

    C++/Train: Make default state for main.cc

--- C++/Train/src/main.cc
+++ C++/Train/src/main.cc
@@ -1,23 +1,9 @@
-#include "IntArray.h"
-#include <iostream>
 
 
 
-int main()
+int main(void)
 {
-    Container::IntArray array { 5, 4, 3, 2, 1 }; // initializer list
-	for (int count{ 0 }; count < array.getLength(); ++count)
-		std::cout << array[count] << ' ';
-
-	std::cout << '\n';
-
-	array = { 1, 3, 5, 7, 9, 11 };
 
 
-	for (int count{ 0 }; count < array.getLength(); ++count)
-		std::cout << array[count] << ' ';
-
-	std::cout << '\n';
-
-	return 0;
+    return 0;
 }
